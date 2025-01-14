6554a5a21daf71ff35fa8b05014ac9d04fdf180f
rashidekbal <57672024+rashidekbal@users.noreply.github.com>
2025-01-15 04:16:33 +0530

    Update ExperiencePage.jsx. Update link of tc lottery

--- src/components/ExperiencePage.jsx
+++ src/components/ExperiencePage.jsx
@@ -4,14 +4,15 @@ import ProjectCard from "./projectCard/ProjectCard";
 function ExperiencePage({ isDark }) {
   let prdetails = [
     {
-      name: "tc lottery",
+      name: "7vbet lottery",
       image: "https://m.media-amazon.com/images/I/A170chtPSXL._SL500_.png",
       info: "Developed a robust and user-friendly online lottery platform, 7vBet, utilizing the MERN stack. The project includes features such as secure user authentication, wallet integration, lottery ticket purchasing, real-time updates, and a responsive UI. Implemented a backend server with Node.js and Express.js for seamless API handling and database operations, while the frontend was built with React.js for an interactive user experience.",
       keyFeatures: [
         "User Registration/Login: Secure authentication and user management.",
         "Wallet Management: Real-time wallet balance updates and transaction logs.",
         "Lottery System: Automated number generation and winner announcement.",
-        "Responsive Design: Optimized for both desktop and mobile devices.",
+        "Responsive Design: Optimized for both desktop and mobile devices.
+        To use it use demo account userId:9123481953, pass:-Rashid786",
       ],
       techUsed: [
         "MySQL",
@@ -21,6 +22,7 @@ function ExperiencePage({ isDark }) {
         "TypeScript",
         "Sass",
       ],
+      link:"https://apkking.xyz",
     },
     {
       name: "UnSplash Wallpaper",
@@ -35,6 +37,7 @@ function ExperiencePage({ isDark }) {
         "Node.js",
         "TypeScript",
       ],
+      link:"apkking.xyz"
     },
   ];
   return (
